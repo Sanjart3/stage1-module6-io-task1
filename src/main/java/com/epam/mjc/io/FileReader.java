d6be4423ee83751fe26cef3347056be6617a134d
stcoderts <115059135+stcoderts@users.noreply.github.com>
2023-06-18 13:04:51 +0500

    small changes2

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -3,16 +3,7 @@ package com.epam.mjc.io;
 import java.io.*;
 import java.util.logging.Logger;
 
-
 public class FileReader {
-
-    public static void main(String[] args) {
-        File file = new File("D:\\java Praktikum\\stage1-module6-io-task1\\src\\resources\\Profile.txt");
-        FileReader fr = new FileReader();
-        Profile profile = fr.getDataFromFile(file);
-        System.out.println(profile.toString());
-    }
-
     public Profile getDataFromFile(File file) {
         Profile profile = new Profile();
         try (BufferedReader bufferedReader = new BufferedReader(new java.io.FileReader(file))){
@@ -24,7 +15,7 @@ public class FileReader {
                     case "Age": profile.setAge(Integer.valueOf(value.trim())); break;
                     case "Email": profile.setEmail(value.trim()); break;
                     case "Phone": profile.setPhone(Long.valueOf(value.trim()));
-                    default: continue;
+                    default:
                 }
                 line = bufferedReader.readLine();
             }
